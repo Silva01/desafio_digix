151a3e0e5cdf8cd7cc56368a811fe5d7233d79d9
silva01 <silva20102@gmail.com>
2023-02-05 11:19:44 -0300

    Adicionado metodo de conversao de string para enum

--- src/main/java/br/net/digix/desafio/silva/daniel/domain/family/value_object/enuns/CriteriaTypeEnum.java
+++ src/main/java/br/net/digix/desafio/silva/daniel/domain/family/value_object/enuns/CriteriaTypeEnum.java
@@ -3,5 +3,14 @@ package br.net.digix.desafio.silva.daniel.domain.family.value_object.enuns;
 public enum CriteriaTypeEnum {
     DEPENDENT_AGE,
     INCOME,
-    IS_WORKING,
+    IS_WORKING;
+
+    public static CriteriaTypeEnum ofCriteria(String type) {
+        for (CriteriaTypeEnum criteriaTypeEnum : CriteriaTypeEnum.values()) {
+            if (criteriaTypeEnum.toString().equals(type)) {
+                return criteriaTypeEnum;
+            }
+        }
+        return null;
+    }
 }
