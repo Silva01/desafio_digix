793f1ae33a4e689c4f71cfdf8488516a0efe8d92
silva01 <silva20102@gmail.com>
2023-02-05 14:39:34 -0300

    Adicionado mapper de dependent ao service

--- src/main/java/br/net/digix/desafio/silva/daniel/infraestrutura/service/FamilyService.java
+++ src/main/java/br/net/digix/desafio/silva/daniel/infraestrutura/service/FamilyService.java
@@ -2,7 +2,9 @@ package br.net.digix.desafio.silva.daniel.infraestrutura.service;
 
 import br.net.digix.desafio.silva.daniel.domain.family.entity.FamilyEntity;
 import br.net.digix.desafio.silva.daniel.domain.family.repository.FamilyRepository;
+import br.net.digix.desafio.silva.daniel.infraestrutura.mapper.DependentToModelMapper;
 import br.net.digix.desafio.silva.daniel.infraestrutura.mapper.FamilyResponseApiMapper;
+import br.net.digix.desafio.silva.daniel.infraestrutura.model.FamilyModel;
 import br.net.digix.desafio.silva.daniel.infraestrutura.repository.FamiliyModelRepository;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +18,21 @@ public class FamilyService implements FamilyRepository {
 
     private final FamilyResponseApiMapper mapper;
 
+    private final DependentToModelMapper dependentMapper;
+
     public FamilyService(final FamiliyModelRepository repository) {
         this.repository = repository;
         this.mapper = new FamilyResponseApiMapper();
+        this.dependentMapper = new DependentToModelMapper();
     }
     @Override
     public void save(FamilyEntity family) {
-        System.out.println("Saving family: " + family);
+        FamilyModel model = new FamilyModel();
+        model.setName(family.getName());
+        model.setIncome(family.getIncome());
+        model.setPoints(family.getPoints());
+        model.setDependents(family.getDependents().stream().map(dependentMapper).collect(Collectors.toList()));
+        repository.save(model);
     }
 
     @Override
