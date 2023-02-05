ee5cacad5b9b39d6cc77a2b849032ee04a782b38
silva01 <silva20102@gmail.com>
2023-02-05 14:31:07 -0300

    refatorado teste de controller

--- src/test/java/br/net/digix/desafio/silva/daniel/infraestrutura/controller/FamilyControllerTest.java
+++ src/test/java/br/net/digix/desafio/silva/daniel/infraestrutura/controller/FamilyControllerTest.java
@@ -1,21 +1,19 @@
 package br.net.digix.desafio.silva.daniel.infraestrutura.controller;
 
-import br.net.digix.desafio.silva.daniel.domain.family.value_object.FamilyDTO;
+import br.net.digix.desafio.silva.daniel.domain.shared.interfaces.EventDispatcherInterface;
+import br.net.digix.desafio.silva.daniel.infraestrutura.service.FamilyService;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.math.BigDecimal;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Tag("integration")
 @WebMvcTest(controllers = FamilyController.class)
@@ -23,6 +21,12 @@ class FamilyControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private FamilyService service;
+
+    @MockBean(name = "familyEventDispatcher")
+    private EventDispatcherInterface eventDispatcher;
 
     @Test
     void shouldCreateFamily() throws Exception {
@@ -35,6 +39,13 @@ class FamilyControllerTest {
 
     @Test
     void shouldGetAllFamilies() throws Exception {
+
+        this.mockMvc.perform(
+                        post("/api/v1/family")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON).content("{\"name\":\"Test\",\"income\":800,\"dependents\":[]}"))
+                .andExpect(status().isCreated());
+
         this.mockMvc.perform(
                 get("/api/v1/family")
                 .contentType(MediaType.APPLICATION_JSON)
