d059975385a0e917ac6f43aafa31bbef3e80e9a7
silva01 <silva20102@gmail.com>
2023-02-05 18:41:27 -0300

    Adicionado mais testes de endpoint

--- src/test/java/br/net/digix/desafio/silva/daniel/infraestrutura/controller/FamilyControllerTest.java
+++ src/test/java/br/net/digix/desafio/silva/daniel/infraestrutura/controller/FamilyControllerTest.java
@@ -9,7 +9,6 @@ import br.net.digix.desafio.silva.daniel.infraestrutura.service.FamilyService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import static org.mockito.Mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -18,9 +17,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Collections;
 import java.util.List;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -79,5 +78,25 @@ class FamilyControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().json("[{\"name\":\"Test\",\"cpf\":\"12345678900\",\"income\":800,\"dependents\":[{\"name\":\"Teste\",\"birthDate\":\"1993-02-05\",\"type\":\"CONJUGE\",\"gender\":\"FEMININO\",\"work\":false}],\"points\":0}]"));
+    }
+
+    @Test
+    void shouldErrorCreateFamilyDontDependent() throws Exception {
+        this.mockMvc.perform(
+                        post("/api/v1/family")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON).content("{\"name\":\"Test\",\"cpf\":\"12345678900\",\"income\":800,\"dependents\":[]}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().json("{\"message\":\"Familia sem conjuge\"}"));
+    }
+
+    @Test
+    void shouldErrorCreateFamilywith2DependentConjuge() throws Exception {
+        this.mockMvc.perform(
+                        post("/api/v1/family")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON).content("{\"name\":\"Test\",\"cpf\":\"12345678900\",\"income\":800,\"dependents\":[{\"name\":\"Teste\",\"birthDate\":\"1993-02-05\",\"type\":\"CONJUGE\",\"gender\":\"FEMININO\",\"work\":false}, {\"name\":\"Teste2\",\"birthDate\":\"1993-02-05\",\"type\":\"CONJUGE\",\"gender\":\"FEMININO\",\"work\":false}]}"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().json("{\"message\":\"Familia com mais de um conjuge\"}"));
     }
 }
