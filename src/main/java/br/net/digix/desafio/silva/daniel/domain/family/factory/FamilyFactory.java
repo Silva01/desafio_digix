a24e934e1218be7794f26831f4df41e48f5e6284
silva01 <silva20102@gmail.com>
2023-02-05 11:22:10 -0300

    Adicionado Factory para criar agregados de family

---
+++ src/main/java/br/net/digix/desafio/silva/daniel/domain/family/factory/FamilyFactory.java
@@ -0,0 +1,11 @@
+package br.net.digix.desafio.silva.daniel.domain.family.factory;
+
+import br.net.digix.desafio.silva.daniel.domain.family.entity.FamilyEntity;
+import br.net.digix.desafio.silva.daniel.domain.family.value_object.FamilyDTO;
+
+public class FamilyFactory {
+
+    public static FamilyEntity createFamilyEntity(final FamilyDTO familyDTO) {
+        return new FamilyEntity(familyDTO.getIncome(), familyDTO.getName(), familyDTO.getDependents());
+    }
+}
