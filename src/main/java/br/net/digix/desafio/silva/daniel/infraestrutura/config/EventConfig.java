815fd6bc366eb4340825fcac2a4e5bf9aada4b77
silva01 <silva20102@gmail.com>
2023-02-05 15:28:18 -0300

    Adicionado evento para salvar familia

--- src/main/java/br/net/digix/desafio/silva/daniel/infraestrutura/config/EventConfig.java
+++ src/main/java/br/net/digix/desafio/silva/daniel/infraestrutura/config/EventConfig.java
@@ -2,6 +2,7 @@ package br.net.digix.desafio.silva.daniel.infraestrutura.config;
 
 import br.net.digix.desafio.silva.daniel.domain.family.factory.FamilyEventFactory;
 import br.net.digix.desafio.silva.daniel.domain.family.repository.CriteriaRepository;
+import br.net.digix.desafio.silva.daniel.domain.family.repository.FamilyRepository;
 import br.net.digix.desafio.silva.daniel.domain.shared.EventDispatcher;
 import br.net.digix.desafio.silva.daniel.domain.shared.interfaces.EventDispatcherInterface;
 import br.net.digix.desafio.silva.daniel.domain.shared.interfaces.EventHandlerInterface;
@@ -22,16 +23,22 @@ public class EventConfig {
 
     private final CriteriaRepository criteriaRepository;
 
+    private final FamilyRepository familyRepository;
+
     @Autowired
-    public EventConfig(CriteriaRepository criteriaRepository) {
+    public EventConfig(CriteriaRepository criteriaRepository, FamilyRepository familyRepository) {
         this.criteriaRepository = criteriaRepository;
+        this.familyRepository = familyRepository;
     }
 
     @Bean(name = "familyEventDispatcher")
     public EventDispatcherInterface eventDispatcher() {
         EventDispatcher eventDispatcher = new EventDispatcher();
-        EventHandlerInterface<EventInterface> handler = FamilyEventFactory.createCalculatePointsHandler(criteriaRepository);
-        eventDispatcher.register(handler, calculatePointsIdentify);
+        EventHandlerInterface<EventInterface> calculateHandler = FamilyEventFactory.createCalculatePointsHandler(criteriaRepository);
+        EventHandlerInterface<EventInterface> saveFamilyHandler = FamilyEventFactory.createSaveFamilyHandler(familyRepository);
+
+        eventDispatcher.register(calculateHandler, calculatePointsIdentify);
+        eventDispatcher.register(saveFamilyHandler, createFamilyIdentify);
         return eventDispatcher;
     }
 }
