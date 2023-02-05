a7c461f17294f6607fcb431b1d4d16fa2cda882c
silva01 <silva20102@gmail.com>
2023-02-05 15:30:35 -0300

    Adicionado Handler para tratativa de erros

---
+++ src/main/java/br/net/digix/desafio/silva/daniel/infraestrutura/exception/handler/GlobalHandler.java
@@ -0,0 +1,19 @@
+package br.net.digix.desafio.silva.daniel.infraestrutura.exception.handler;
+
+
+import br.net.digix.desafio.silva.daniel.infraestrutura.dto.ErrorResponseDTO;
+import br.net.digix.desafio.silva.daniel.infraestrutura.exception.CpfJaCadastradoException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalHandler {
+
+    @ExceptionHandler(value = {CpfJaCadastradoException.class})
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorResponseDTO cpfNaoCadastradoHandler(CpfJaCadastradoException ex) {
+        return new ErrorResponseDTO("Familia Já Cadastrada");
+    }
+}
