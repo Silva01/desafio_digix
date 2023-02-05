ccbc0bb22266f4ba4823c7c1dde5957248d93aea
silva01 <silva20102@gmail.com>
2023-02-05 15:29:36 -0300

    Adicionado criacao de evento para salvar familia

--- src/main/java/br/net/digix/desafio/silva/daniel/domain/family/factory/FamilyEventFactory.java
+++ src/main/java/br/net/digix/desafio/silva/daniel/domain/family/factory/FamilyEventFactory.java
@@ -3,7 +3,9 @@ package br.net.digix.desafio.silva.daniel.domain.family.factory;
 import br.net.digix.desafio.silva.daniel.domain.family.entity.FamilyEntity;
 import br.net.digix.desafio.silva.daniel.domain.family.event.FamilyEvent;
 import br.net.digix.desafio.silva.daniel.domain.family.event.handler.CalculatePointsFamilyHandler;
+import br.net.digix.desafio.silva.daniel.domain.family.event.handler.SaveFamilyHandler;
 import br.net.digix.desafio.silva.daniel.domain.family.repository.CriteriaRepository;
+import br.net.digix.desafio.silva.daniel.domain.family.repository.FamilyRepository;
 import br.net.digix.desafio.silva.daniel.domain.family.service.CalculateDependentFamilyService;
 import br.net.digix.desafio.silva.daniel.domain.family.service.CalculateIncomeFamilyService;
 import br.net.digix.desafio.silva.daniel.domain.shared.interfaces.EventHandlerInterface;
@@ -20,6 +22,10 @@ public class FamilyEventFactory {
                         new CalculateDependentFamilyService(),
                         new CalculateIncomeFamilyService()
                 ));
+    }
+
+    public static EventHandlerInterface<EventInterface> createSaveFamilyHandler(final FamilyRepository familyRepository) {
+        return new SaveFamilyHandler(familyRepository);
     }
 
     public static EventInterface createEventFamily(final FamilyEntity familyEntity) {
