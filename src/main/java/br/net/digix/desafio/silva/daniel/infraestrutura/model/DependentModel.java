293c2e6b095cb8aab0781bb5179ce6ac5fecd099
silva01 <silva20102@gmail.com>
2023-02-05 14:32:17 -0300

    Adicionado model de dependentes

---
+++ src/main/java/br/net/digix/desafio/silva/daniel/infraestrutura/model/DependentModel.java
@@ -0,0 +1,77 @@
+package br.net.digix.desafio.silva.daniel.infraestrutura.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "dependent")
+public class DependentModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "is_working")
+    private boolean isWorking;
+
+    @Column(name = "type")
+    private String type;
+
+    @Column(name = "gender")
+    private String gender;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isWorking() {
+        return isWorking;
+    }
+
+    public void setWorking(boolean working) {
+        isWorking = working;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
